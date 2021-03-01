73f514cf7ba903686a0d167107b46682dbfe644a
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-01 22:44:32 +0900

    Create dashboard component showing tweet ids

---
+++ src/components/Dashboard.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <h3 className='center'>Your Timeline</h3>
+                <ul className='dashboard-list'>
+                    {this.props.tweetsIds.map((tweetId) => (
+                        <li key={tweetId}>
+                            <div>TWEET ID: {tweetId}</div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps({ tweets }) {
+    return {
+        tweetsIds: Object.keys(tweets)
+            .sort((a,b) => tweets[b].timestamp - tweets[a].timestamp)
+    }
+}
+
+export default connect(mapStateToProps)(Dashboard)
